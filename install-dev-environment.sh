5ef3ae5a6eee61c7c5140940bab3cc27dba05c2a
Sercan Akmaz <sercan.akmaz@hepsiburada.com>
2019-05-14 11:25:05 +0300

    -

--- install-dev-environment.sh
+++ install-dev-environment.sh
@@ -138,5 +138,5 @@
 
 ######## Inotify Watches Limit
 
-sudo echo "fs.inotify.max_user_watches = 524288" >> "/etc/sysctl.conf"
-sysctl -p --system
+# sudo echo "fs.inotify.max_user_watches = 524288" >> "/etc/sysctl.conf"
+# sudo sysctl -p --system
