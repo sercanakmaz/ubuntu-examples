f1a0d07804cb1bd5c900978057ed08a0aab4ed12
Sercan Akmaz <sercan.akmaz@hepsiburada.com>
2019-05-17 14:52:17 +0300

    -

--- install-dev-environment.sh
+++ install-dev-environment.sh
@@ -26,7 +26,6 @@
 # sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 # echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 # sudo apt update
-
 # sudo apt install mono-complete
 
 ######## Git
@@ -138,6 +137,10 @@
 # sudo apt install python-pip
 # sudo apt install python3-pip
 # sudo apt-get install python3.6
+
+######## Redis Desktop Manager
+
+# sudo snap install redis-desktop-manager
 
 ######## Inotify Watches Limit
 
